b90ea7abe2290b0060552a0f5aee19f14221de37
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2020-05-11 21:22:09 +0100

    create service and connect with json bin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  requestOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Secret-key': '$2b$10$YMex2SD7OxoCHvaVxN2bpOATI3DlmoNQRPeTBMr6rJ4zg.egEpZmG',
+      'Collecyion-id': '5eb869fa47a2266b14763f6b'
+    }
+  }
+
+  constructor(private readonly httpService: HttpClient) { }
+
+  postItem(data: any): Observable<any> {
+    const url = 'https://api.jsonbin.io/b';
+    return this.httpService.post(url, data, this.requestOptions);
+  }
+}
