d7769bbbd8254a1ec27e9b33264028962090ffc0
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2020-05-13 00:46:49 +0100

    add ability to edit the whole list, save changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormBuilder, FormArray } from '@angular/forms';
 import { DataService } from './data.service';
 
 @Component({
@@ -7,29 +7,56 @@ import { DataService } from './data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  constructor(private readonly dataService: DataService){}
+  constructor(
+    private readonly dataService: DataService,
+    private readonly fb: FormBuilder
+  ) { }
 
   title = 'shopping-list';
+  isLoading = false;
 
-  items: any[];
-  newItem = new FormControl('');
+  itemsArrayForm = this.fb.group({
+    items: this.fb.array([])
+  });
 
   ngOnInit(): void {
-    this.getData()
+    this.populateForm()
   }
 
   onClick() {
-    this.dataService.postItem([...this.items].concat(this.newItem.value))
-      .subscribe((response) => {
-        console.log('done', response);
-        this.getData();
-      });
-  }         
+    this.isLoading = true;
+    this.dataService.postItem(this.items.value.filter(value => value))
+      .subscribe(
+        () => this.populateForm(),
+        error => {
+          console.log(error);
+          this.isLoading = false;
+        }
+      )
+  }
 
-  getData() {
+  onKeydown(event, index){
+    this.items.insert(index + 1, new FormControl(''))
+  }
+
+  get items(): FormArray {
+    return this.itemsArrayForm.controls.items as FormArray;
+  }
+
+  populateForm() {
     this.dataService.getItems()
-      .subscribe(response => this.items = response);
+      .subscribe(response => {
+        this.items.clear()
+        response.forEach(element => {
+          this.items.push(new FormControl(element));
+        });
+        this.items.push(new FormControl(''))
+        this.isLoading = false;
+      }, error => {
+        console.log(error);
+        this.isLoading = false;
+      })
   }
 }
