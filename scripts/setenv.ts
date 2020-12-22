4eff2497873cdf9682ccda2792839e96dc377f42
Miguel <miguel.moreno.baladron@gmail.com>
2020-12-22 17:51:14 +0000

    Write secrets to different file

--- scripts/setenv.ts
+++ scripts/setenv.ts
@@ -2,9 +2,10 @@ const { writeFile } = require('fs');
 const { argv } = require('yargs');// read environment variables from .env file
 require('dotenv').config();// read the command line arguments passed with yargs
 const environment = argv.environment;
-const isProduction = environment === 'prod'; const targetPath = isProduction
-    ? `./src/environments/environment.prod.ts`
-    : `./src/environments/environment.ts`;// we have access to our environment variables
+const isProduction = environment === 'prod';
+const targetPath = "./src/environments/secrets.ts"
+
+// we have access to our environment variables
 // in the process.env object thanks to dotenv
 const environmentFileContent = `
 export const environment = {
